7e9183cc2e6dd371803e740146502ba90cfea9d2
marianaf <mlfernandez@hotmail.com>
2021-05-19 16:16:14 +0200

    changes endopont from movies search by title

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -4,14 +4,17 @@ class Peliculas {
   // metodos de funciones controladoras
   async findTopRated() {
     let res = await axios.get(
-      `https://api.themoviedb.org/3/genre/movie/list?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`
+      "https://api.themoviedb.org/3/movie/top_rated?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US&page=1"
     );
+
     return res.data;
   }
+
   async searchById(id) {
     let res = await axios.get(
       `https://api.themoviedb.org/3/movie/${id}?api_key=210d6a5dd3f16419ce349c9f1b200d6d&language=en-US`
     );
+
     return res.data;
   }
 
@@ -21,9 +24,7 @@ class Peliculas {
     );
     return res.data;
   }
-
 }
 
 let moviesController = new Peliculas();
 module.exports = moviesController;
-
