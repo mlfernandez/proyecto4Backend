ca644f83725d68eeaea5811c9aafb247238fe5c1
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-05-19 13:09:00 +0200

    add file index,js / router.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const router = require('./router')
+
+//middewares
+
+app.use(express.json());
+app.use(router);
+app.listen(port,()=> console.log(`Node server up to http://localhost:${port}`))
