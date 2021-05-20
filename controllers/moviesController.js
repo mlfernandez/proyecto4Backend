a758b470929e529c72acedfd4a956f94f46386bf
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-05-20 17:13:16 +0200

    Importing the checkId function from the utiles.js folder to moviesControler.js

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,5 +1,7 @@
 const axios = require("axios");
 
+const checkId = require("../utiles");
+
 class Peliculas {
   // metodos de funciones controladoras
   async findTopRated() {
@@ -26,102 +28,14 @@ class Peliculas {
   }
 
   async searchByGenre(nombre) {
-    let code = checkId(nombre, traductorGenero);
+    // checkId(nombre,traductorGenero)
+    let code = checkId(nombre);
     let res = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?api_key=210d6a5dd3f16419ce349c9f1b200d6d&with_genres=${code}`
     );
-    console.log(nombre);
     return res.data;
   }
 }
-// array con los generos por nombre y id
-let traductorGenero = [
-  {
-    id: 28,
-    name: "Action",
-  },
-  {
-    id: 12,
-    name: "Adventure",
-  },
-  {
-    id: 16,
-    name: "Animation",
-  },
-  {
-    id: 35,
-    name: "Comedy",
-  },
-  {
-    id: 80,
-    name: "Crime",
-  },
-  {
-    id: 99,
-    name: "Documentary",
-  },
-  {
-    id: 18,
-    name: "Drama",
-  },
-  {
-    id: 10751,
-    name: "Family",
-  },
-  {
-    id: 14,
-    name: "Fantasy",
-  },
-  {
-    id: 36,
-    name: "History",
-  },
-  {
-    id: 27,
-    name: "Horror",
-  },
-  {
-    id: 10402,
-    name: "Music",
-  },
-  {
-    id: 9648,
-    name: "Mystery",
-  },
-  {
-    id: 10749,
-    name: "Romance",
-  },
-  {
-    id: 878,
-    name: "Science Fiction",
-  },
-  {
-    id: 10770,
-    name: "TV Movie",
-  },
-  {
-    id: 53,
-    name: "Thriller",
-  },
-  {
-    id: 10752,
-    name: "War",
-  },
-  {
-    id: 37,
-    name: "Western",
-  },
-];
-
-// bucle for
-const checkId = (nombre, array) => {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i].name === nombre) {
-      return array[i].id;
-    }
-  }
-};
 
 let moviesController = new Peliculas();
 module.exports = moviesController;
