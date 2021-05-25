f75057b31b5bf99188aa8fbadf1a0f3fd9f1d4e4
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-05-25 13:51:53 +0200

    server concection and DataBase working perfect

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const db = require("./db")
 const app = express();
 const port = 3000;
-
 const router = require("./router");
 
 //middelwares
 
 app.use(express.json());
 app.use(router);
-app.listen(port, () =>
-  console.log(`Node server up to http://localhost:${port}`)
-);
+db
+.then(()=>{
+  //Starting server
+
+   app.listen(port, ()=> console.log(`Servidor levantado en http://localhost:${port}`));
+  })
+  .catch((err)=> console.log(err.message));   
