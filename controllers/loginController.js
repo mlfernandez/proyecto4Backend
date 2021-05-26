d1cb10e8903624ddc351425ce19f60102a1746ff
marianaf <mlfernandez@hotmail.com>
2021-05-26 17:50:29 +0200

    create login router and login controller

---
+++ controllers/loginController.js
@@ -0,0 +1,31 @@
+const userController = require('./usersController');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secret = "Esto es lo mas dificil del bootcamp";
+
+class LoginController {
+    async validate(emailCheck,passwordCheck){
+
+        let user = await userController.emailUser(passwordCheck);
+
+        let password = user.password;
+
+        let verificar = await bcryptjs.compare(passwordCheck,password);
+
+        if(!verificar){
+            return new Error("El password o el email no coinciden");
+            
+        }
+
+        let payload = {
+            userId : user.id,
+            createdAt: new Date,
+        };
+
+        return jwt.sign(payload,secret);
+
+    }
+}
+
+let loginController = new LoginController();
+module.exports = loginController;
