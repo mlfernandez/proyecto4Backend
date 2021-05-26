a7e2bda60874921e392a4a118b426ef5b2eb4aac
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-05-26 17:29:46 +0200

    create CRUD order and user

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -1,29 +1,32 @@
-const { Order } = require ('../models');
-const router = require('../routes/ordersRouters');
-
+const { Order } = require("../models");
+const router = require("../routes/ordersRouters");
 
 class Pedido {
 
-    async allOrders(){
-        return Order.findAll();
-    }
-    async OrderId() {
-        return Order.findByPk(id);
-    }
-    async newOrder(body) {
-        return Order.create(body);
-    }
-    async modifyOrder(cuerpoDeDatos) {
-        return Order.update (
-            //datos que cambiamos
-            {idMovie: cuerpoDeDatos.idMovie, rentalDate: cuerpoDeDatos.rentalDate},
-            //donde
-            {where: {id: cuerpoDeDatos.id}}
-        )
-    }
-    async deleteOrder(id) {
-        return Order.destroy ({where: {id: id}})
-    }
+  async allOrders() {
+    return Order.findAll();
+  }
+  async OrderId() {
+    return Order.findByPk(id);
+  }
+  async newOrder(body) {
+    return Order.create(body);
+  }
+  async modifyOrder(cuerpoDeDatos) {
+
+    return Order.update(
+
+      //datos que cambiamos
+      { idMovie: cuerpoDeDatos.idMovie, rentalDate: cuerpoDeDatos.rentalDate },
+
+      //donde
+      
+      { where: { id: cuerpoDeDatos.id } }
+    );
+  }
+  async deleteOrder(id) {
+    return Order.destroy({ where: { id: id } });
+  }
 }
 let orderController = new Pedido();
 module.exports = orderController;
--- router.js
+++ router.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 
 const movieRouter = require("./routes/moviesRouter");
 const seriesRouter = require("./routes/seriesRouter");
-const usersRouter= require('./routes/usersRouter');
-const orderRouter= require('./routes/orderRouter')
+const usersRouter = require('./routes/usersRouter');
+const orderRouter = require('./routes/ordersRouters')
 
 router.use("/movies", movieRouter);
 router.use("/series", seriesRouter);
 router.use('/users', usersRouter);
-router.use('/orders',orderRouter);
+router.use('/orders', orderRouter);
 
 
 module.exports = router;
