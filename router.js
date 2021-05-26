81502f204a553945a043a83a60cac3e2b1b3402d
Carlos Quintero <CarlosQuintero92.cq@gmail.com>
2021-05-26 16:38:52 +0200

    creando ordersControllers

--- router.js
+++ router.js
@@ -2,10 +2,13 @@ const router = require("express").Router();
 
 const movieRouter = require("./routes/moviesRouter");
 const seriesRouter = require("./routes/seriesRouter");
-const usersRouter= require('./routes/usersRouter')
+const usersRouter= require('./routes/usersRouter');
+const orderRouter= require('./routes/orderRouter')
 
 router.use("/movies", movieRouter);
 router.use("/series", seriesRouter);
 router.use('/users', usersRouter);
+router.use('/orders',orderRouter);
+
 
 module.exports = router;
