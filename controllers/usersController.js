bca3e328e3bacf8f5bd9b220c008cd41bfc61d9e
marianaf <mlfernandez@hotmail.com>
2021-05-26 18:05:18 +0200

    working crud users with bcryptjs and authenticate

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,41 @@
-//const axios = require("axios"); al no usar endpoints creo que no lo necesitamos
-
-//const checkId = require("../utiles"); al no tener funciones en utiles no lo pondria
-
 const { User } = require("../models");
+const router = require("../routes/usersRouter");
 
 class Person {
-
-    async findAllUsers(){
-        return User.findAll();
-    }
-
-    async signUpUser(user){
-        
-        return User.create(user);
-    }
-
+  async findAllUsers() {
+    return User.findAll();
+  }
+  async findByEmail(email) {
+    return User.findOne({
+      where: { email },
+    });
+  }
+  async findByUserId() {
+    return User.findByPk(id);
+  }
+  async newUser(body) {
+    let password = body.password;
+    let passwordHashed = bcryptjs.hashSync(password, 10);
+    body.password = passwordHashed;
+    return User.create(body);
+  }
+  async modifyUser(cuerpoDeDatos) {
+    return User.update(
+      //datos que cambiamos
+      {
+        billing_address: cuerpoDeDatos.billing_address,
+        shipping_address: cuerpoDeDatos.shipping_address,
+        phone: cuerpoDeDatos.phone,
+        city: cuerpoDeDatos.city,
+        country: cuerpoDeDatos.country,
+      },
+      //donde
+      { where: { id: cuerpoDeDatos.id } }
+    );
+  }
+  async deleteUser(id) {
+    return User.destroy({ where: { id: id } });
+  }
 }
-
-
 let usersController = new Person();
 module.exports = usersController;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,28 +1,73 @@
-const router = require('express').Router();
-const usersController = require('../controllers/usersController');
+const router = require("express").Router();
+const authenticate = require("../../../sequelize/dia2/middleware/authenticate");
+const usersController = require("../controllers/usersController");
 
 //CRUD
 
-router.get('/', async(req, res) => {
-    try {
-        res.json(await usersController.findAllUsers())
-    } catch (err) {
-        return res.status(500).json({
-            message: err.message
-        });
-    }
+router.get("/", async (req, res) => {
+  try {
+    res.json(await usersController.findAllUsers());
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
 });
 
-router.post('/', async(req, res) => {
-    try {
-        const id = await usersController.signUpUser(req.body);
-        const status = 'success';
-        res.json({ status, id });
-    } catch (err) {
-        return res.status(500).json({
-            message: err.message
-        });
-    }
-})
+router.get("/email/:email", async (req, res) => {
+  try {
+    res.json(await usersController.findByEmail());
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+router.get("/:id", authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    res.json(await pasajeroController.findByUserId(id));
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const body = req.body;
+
+    res.json(await pasajeroController.newUser(body));
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+router.put("/", authenticate, async (req, res) => {
+  try {
+    const cuerpoDeDatos = req.body;
+    res.json(await pasajeroController.modifyUser(cuerpoDeDatos));
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
+
+router.delete("/:id", authenticate, async (req, res) => {
+  try {
+    const id = req.params.id;
+    res.json(await pasajeroController.deleteUser(id));
+  } catch (err) {
+    return res.status(500).json({
+      message: err.message,
+    });
+  }
+});
 
 module.exports = router;
