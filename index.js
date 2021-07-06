4068ab5ff0ca8c6ca6f4ab3745c67c06549515c8
mlfernandez <mlfernandez@gmail.com>
2021-07-06 16:56:32 +0200

    conectando heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const db = require("./db")
 const app = express();
-const port = 3005;
+const port = process.env.PORT || 3005; 
 const router = require("./router");
 const cors = require('cors');
 
